6221da3b9ca031c94e4992522e7bbf4899ff312f
AizatD <aizatdoku@gmail.com>
2024-07-04 01:31:26 +0000

    added class2 files

---
+++ Class2/group.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_group" "developer" {
+  name = "developer"
+  
+}
+resource "aws_iam_group" "developers2" {
+  name = "hello"
+  
+}
+resource "aws_iam_group" "developers3" {
+  name = "developers3"
+  
+}
